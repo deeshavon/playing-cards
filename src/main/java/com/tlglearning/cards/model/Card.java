be7ebb1ce49b0bba642ef96e0dbaedda8766f08c
deeshavon <deja.greene@tlgcohort.com>
2022-09-16 15:32:46 -0700

    Enhanced Card implementation, started Deck.

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -1,7 +1,9 @@
 package com.tlglearning.cards.model;
 //import statements are always after the package code line (#1) above
 
-public class Card {
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
 
   private final Rank rank;
   private final Suit suit;
@@ -20,10 +22,35 @@ public class Card {
   }
 
   @Override
+  public int hashCode() {
+    return Objects.hash(rank, suit); // FIXME: 9/16/22 Move this to initialization. 
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result;
+    if (this == obj) {
+      result = true;
+    } else if (obj instanceof Card) {
+      Card other = (Card) obj; 
+      result = (this.rank == other.rank && this.suit == other.suit);
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
   }
 
+  @Override
+  public int compareTo(Card other) {
+    return 0; // TODO: 9/16/22 Replace with comparison based on suit and rank. 
+  }
+
   // TODO: 9/13/22 Override equals method. 
-  // TODO: 9/13/22 Override hashCode method. 
+  // TODO: 9/13/22 Override hashCode method.
+
 }
